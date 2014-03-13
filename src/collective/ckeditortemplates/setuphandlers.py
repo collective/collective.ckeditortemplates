3a950ad1dd8ffff5c865dc389f3ad5864bfeff10
Benoît Suttor <benoit.suttor@imio.be>
2014-03-13 16:46:17 +0100

    remove global allow folder

--- src/collective/ckeditortemplates/setuphandlers.py
+++ src/collective/ckeditortemplates/setuphandlers.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from Products.CMFCore.utils import getToolByName
 from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
 
-FODLER="ckeditortemplates"
+FOLDER="ckeditortemplates"
 
 def setupVarious(context):
     site = context.getSite()
     if context.readDataFile('collective.ckeditortemplates_various.txt') is None:
         return
 
-    if not api.content.get(FODLER):
-        container = api.content.create(site, "cktemplatefolder", title=FODLER)
+    if not api.content.get(FOLDER):
+        types = getToolByName(site, 'portal_types')
+        types.getTypeInfo('cktemplatefolder').global_allow = True
+        container = api.content.create(site, "cktemplatefolder", id=FOLDER, title=FOLDER)
         excl = IExcludeFromNavigation(container)
         excl.exclude_from_nav = True
+        types.getTypeInfo('cktemplatefolder').global_allow = False
         container.reindexObject()
+        container.unindexObject()
