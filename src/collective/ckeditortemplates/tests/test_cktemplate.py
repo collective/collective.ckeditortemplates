4d7835618e766e1e548343f346256f96a571a993
Martin Peeters <martin.peeters@affinitic.be>
2014-03-27 08:56:36 +0100

    Add tests for the parsing of html. refs #7744

--- src/collective/ckeditortemplates/tests/test_cktemplate.py
+++ src/collective/ckeditortemplates/tests/test_cktemplate.py
@@ -38,3 +38,17 @@ class TestCKTemplate(unittest2.TestCase):
         self.assertFalse(self.restricted_template.can_view(self.portal))
         self.assertTrue(self.normal_template.can_view(self.portal))
         logout()
+
+    def test_html_simple(self):
+        template = cktemplate.CKTemplate()
+        content = """<h1>My title</h1><p>Lorem ipsum dolor..."""
+        template.content = type('content', (object, ), {'raw': content})
+        html = """<h1>My title</h1><p>Lorem ipsum dolor...</p>"""
+        self.assertEqual(html, template.html)
+
+    def test_html_complex(self):
+        template = cktemplate.CKTemplate()
+        content = """<p class="a">text1'<a href="b">"text2"</a></p>"""
+        template.content = type('content', (object, ), {'raw': content})
+        html = """<p class='a'>text1'<a href='b'>&quot;text2&quot;</a></p>"""
+        self.assertEqual(html, template.html)
