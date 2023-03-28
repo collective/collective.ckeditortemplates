eb69b5695ff72f22e551d4f9def3a97991b56746
Stephan Geulette <s.geulette@imio.be>
2023-03-28 15:23:59 +0200

    Updated test to work both sides

--- src/collective/ckeditortemplates/browser/tests/test_cktemplatelisting.py
+++ src/collective/ckeditortemplates/browser/tests/test_cktemplatelisting.py
@@ -2,6 +2,11 @@
 from collective.ckeditortemplates.browser import cktemplatelisting
 from collective.ckeditortemplates.testing import CKTEMPLATES_INTEGRATION_TESTING
 from mock import Mock
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.app.textfield import RichTextValue
+from Products.CMFPlone.utils import safe_unicode
 from zope.publisher.browser import TestRequest
 
 import unittest
@@ -29,14 +34,20 @@ class TestCKTemplateListingView(unittest.TestCase):
         self.assertEqual(render, self.view.render())
 
     def test_render_template(self):
-        template = type('template', (object, ), {
-            u'title': u'My "title"',
-            u'custom_icon': True,
-            u'image': lambda: 'icon.jpg',
-            u'description': u'Template "description"',
-            u'html': lambda: u'<h1>My title</h1>'})
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        template = api.content.create(self.portal.ckeditortemplates, 'cktemplate', 'test1', 'My &quot;title&quot;',
+                                      description="Template &quot;description&quot;",
+                                      content=RichTextValue(raw=safe_unicode(u'<h1>My title</h1>'),
+                                                            mimeType=u"text/html", outputMimeType=u"text/x-html-safe"))
+        # the following is not working on python 2
+        # template = type('template', (object, ), {
+        #     u'title': u'My "title"',
+        #     u'custom_icon': True,
+        #     u'image': lambda: 'icon.jpg',
+        #     u'description': u'Template "description"',
+        #     u'html': lambda: u'<h1>My title</h1>'})
         render = (u'{title: "My &quot;title&quot;", '
-                  u'image: "/template/icon.jpg", '
+                  # u'image: "/template/icon.jpg", '
                   u'description: "Template &quot;description&quot;", '
                   u'html: "<h1>My title</h1>"}')
         self.assertEqual(render, self.view.render_template(template, u'/template'))
